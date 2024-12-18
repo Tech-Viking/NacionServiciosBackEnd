2ac560e4fe950fb5d54e04034ecf206c8ab6283d
Viking Tech <mjm.mercado@gmail.com>
2024-12-18 16:26:16 -0300

    cambios apra probar otras config swagger

--- src/main/java/sube/interviews/mareoenvios/config/OpenApiConfig.java
+++ src/main/java/sube/interviews/mareoenvios/config/OpenApiConfig.java
@@ -1,18 +1,21 @@
 package sube.interviews.mareoenvios.config;
 
-  import io.swagger.v3.oas.models.OpenAPI;
-  import io.swagger.v3.oas.models.info.Info;
-  import org.springframework.context.annotation.Bean;
-  import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
- @Configuration
- public class OpenApiConfig {
-      @Bean
-      public OpenAPI customOpenAPI() {
-          return new OpenAPI()
-                 .info(new Info()
-                 .title("Mareo Envíos API")
-                 .version("v1")
-                  .description("API para la gestión de envíos de mercadería"));
-      }
- }
+@Configuration
+public class OpenApiConfig {
+  @Bean
+  public OpenAPI customOpenAPI() {
+      return new OpenAPI()
+              .info(new Info()
+                      .title("Mareo Envíos API")
+                      .version("v1")
+                      .description("API para la gestión de envíos de mercadería"))
+              .servers(List.of(new Server().url("/")));
+  }
+}
